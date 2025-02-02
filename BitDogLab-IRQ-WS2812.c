884dfd4b4a8c645975027f9b4ac4eda70bb1c571
hsantosdias@gmail.com <hsantosdias@gmail.com>
2025-02-01 23:53:54 -0300

    Teste de impressão da matriz 5x5 de 1 a 9

--- BitDogLab-IRQ-WS2812.c
+++ BitDogLab-IRQ-WS2812.c
@@ -78,6 +78,107 @@ void exibir_numero(int numero) {
     printf("Número exibido: %d\n", numero);
 }
 
+/*
+void exibir_numero_matriz(int numero) {
+    const uint8_t numeros[10][5][5] = {
+        // 0
+        {{1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 1},
+         {1, 0, 0, 0, 1},
+         {1, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1}},
+        // 1
+        {{0, 0, 1, 0, 0},
+         {0, 1, 1, 0, 0},
+         {1, 0, 1, 0, 0},
+         {0, 0, 1, 0, 0},
+         {1, 1, 1, 1, 1}},
+        // 2
+        {{1, 1, 1, 1, 1},
+         {0, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 0},
+         {1, 1, 1, 1, 1}},
+        // 3
+        {{1, 1, 1, 1, 1},
+         {0, 0, 0, 0, 1},
+         {0, 1, 1, 1, 1},
+         {0, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1}},
+        // 4
+        {{1, 0, 0, 1, 1},
+         {1, 0, 0, 1, 1},
+         {1, 1, 1, 1, 1},
+         {0, 0, 0, 1, 1},
+         {0, 0, 0, 1, 1}},
+        // 5
+        {{1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 0},
+         {1, 1, 1, 1, 1},
+         {0, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1}},
+        // 6
+        {{1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 0},
+         {1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1}},
+        // 7
+        {{1, 1, 1, 1, 1},
+         {0, 0, 0, 0, 1},
+         {0, 0, 0, 1, 1},
+         {0, 0, 1, 1, 1},
+         {0, 0, 1, 1, 1}},
+        // 8
+        {{1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1}},
+        // 9
+        {{1, 1, 1, 1, 1},
+         {1, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1},
+         {0, 0, 0, 0, 1},
+         {1, 1, 1, 1, 1}}
+    };
+    
+    printf("Número na matriz 5x5:\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", numeros[numero][i][j]);
+        }
+        printf("\n");
+    }
+}
+
+*/
+void exibir_numero_matriz(int numero) {
+    const uint8_t numeros[10][5][5] = {
+        {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}},
+        {{0, 0, 1, 0, 0}, {0, 1, 1, 0, 0}, {1, 0, 1, 0, 0}, {0, 0, 1, 0, 0}, {1, 1, 1, 1, 1}},
+        {{1, 1, 1, 1, 1}, {0, 0, 0, 0, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 0, 0}, {1, 1, 1, 1, 1}},
+        {{1, 1, 1, 1, 1}, {0, 0, 0, 0, 1}, {0, 1, 1, 1, 1}, {0, 0, 0, 0, 1}, {1, 1, 1, 1, 1}},
+        {{1, 0, 0, 1, 1}, {1, 0, 0, 1, 1}, {1, 1, 1, 1, 1}, {0, 0, 0, 1, 1}, {0, 0, 0, 1, 1}},
+        {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 1}, {1, 1, 1, 1, 1}},
+        {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}},
+        {{1, 1, 1, 1, 1}, {0, 0, 0, 0, 1}, {0, 0, 0, 1, 1}, {0, 0, 1, 1, 1}, {0, 0, 1, 1, 1}},
+        {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}},
+        {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 1}, {1, 1, 1, 1, 1}}
+    };
+    
+    printf("Número na matriz 5x5:\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            matrix_leds[i][j].red = numeros[numero][i][j] ? 255 : 0;
+            printf("%d ", numeros[numero][i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
 // Função para atualizar os estados dos LEDs
 void set_led_color(struct LEDPins leds, bool R, bool G, bool B) {
     gpio_put(leds.red, R);
@@ -92,6 +193,28 @@ bool repeating_timer_callback(struct repeating_timer *t) {
     gpio_put(led_pins.red, led_on);
     return true;
 }
+
+int main() {
+    stdio_init_all();
+    gpio_init(button_pins.button_a);
+    gpio_set_dir(button_pins.button_a, GPIO_IN);
+    gpio_pull_up(button_pins.button_a);
+    gpio_set_irq_enabled_with_callback(button_pins.button_a, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    
+    gpio_init(button_pins.button_b);
+    gpio_set_dir(button_pins.button_b, GPIO_IN);
+    gpio_pull_up(button_pins.button_b);
+    gpio_set_irq_enabled_with_callback(button_pins.button_b, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    
+    while (true) {
+        exibir_numero_matriz(numero_exibido);
+        sleep_ms(TEMPO);
+    }
+    return 0;
+}
+
+
+/*
 
 int main() {
     stdio_init_all();
@@ -124,3 +247,6 @@ int main() {
     }
     return 0;
 }
+
+
+*/
